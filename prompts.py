1b2bc7771a5892c30fecb6da5e0a08d9b3792830
raghukotha <raghu_kotha@yahoo.com>
2020-04-30 19:37:27 -0400

    Modified crate reminder text

--- prompts.py
+++ prompts.py
@@ -3,4 +3,4 @@ Welcome to KUVAN Assist. I am Your personal assistant and am here to help suppor
 you with your KUVAN Treatment.  This setup exercise will take less than five minutes. 
 Let us get started. You can get started by saying I am now taking medication KUVAN.
 """
-KUVAN_YOU_WANT_METO_REMIND = 'So you want me to remind about medication. Is that right?'
+KUVAN_YOU_WANT_METO_REMIND = 'So you want me to remind about medication. Is that right? If so please say "Create reminder".'
