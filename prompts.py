550f8059343960895a7a3186f3de55f0d3b5180a
raghukotha <raghu_kotha@yahoo.com>
2020-04-30 15:31:56 -0400

    Initial Commit

---
+++ prompts.py
@@ -0,0 +1,6 @@
+KUVAN_WELCOME_MESSAGE = """
+Welcome to KUVAN Assist. I am Your personal assistant and am here to help support 
+you with your KUVAN Treatment.  This setup exercise will take less than five minutes. 
+Let us get started. You can get started by saying I am now taking medication KUVAN.
+"""
+KUVAN_YOU_WANT_METO_REMIND = 'So you want me to remind about medication. Is that right?'
