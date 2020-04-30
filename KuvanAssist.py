669c65946b43f96dd65ed6a27780ef5523542084
raghukotha <raghu_kotha@yahoo.com>
2020-04-30 16:30:09 -0400

    Fixed logger bug

--- KuvanAssist.py
+++ KuvanAssist.py
@@ -25,14 +25,14 @@ class LaunchRequestHandler(AbstractRequestHandler):
     """Handler for Skill Launch."""
     def can_handle(self, handler_input):
         # type: (HandlerInput) -> bool
-        logger('In LaunchRequestHandler Can Handle')
+        logger.info('In LaunchRequestHandler Can Handle')
 
         return ask_utils.is_request_type("LaunchRequest")(handler_input)
 
     def handle(self, handler_input):
         # type: (HandlerInput) -> Response
         speak_output = prompts.KUVAN_WELCOME_MESSAGE
-        logger('In LaunchRequestHandler {speak_output}')
+        logger.info('In LaunchRequestHandler {speak_output}')
 
         return (
             handler_input.response_builder
